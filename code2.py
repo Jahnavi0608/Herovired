6046011647e4b743810600444e8827294e0cc747
Jahnavi0608 <84060582+Jahnavi0608@users.noreply.github.com>
2022-12-11 15:23:30 +0530

    Update code2.py

--- code2.py
+++ code2.py
@@ -2,3 +2,13 @@ cars={"Maruthi":"red","Ford":"Blue","Suzuki":"Black","Tesla":"White","Tata Motor
 print(cars)
 print(cars.keys())
 print(cars.values())
+
+
+
+'''
+
+{'Maruthi': 'red', 'Ford': 'Blue', 'Suzuki': 'Black', 'Tesla': 'White', 'Tata Motors': 'Green'}
+dict_keys(['Maruthi', 'Ford', 'Suzuki', 'Tesla', 'Tata Motors'])
+dict_values(['red', 'Blue', 'Black', 'White', 'Green'])
+
+'''
